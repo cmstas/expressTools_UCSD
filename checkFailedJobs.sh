579b7b3202902e9acb2292d16b92ac16aefe0471
yanjuntu <yanjuntu>
2011-03-24 23:44:20 +0000

    add auto resubmitting

--- checkFailedJobs.sh
+++ checkFailedJobs.sh
@@ -1,40 +1,56 @@
-
+cur_dir=$PWD
 dataset_name=$1
 cms2_tag=$2
 dataset_dir_tmp=`echo $dataset_name |sed -e 's?/?_?g' `
 dataset_dir=`echo ${dataset_dir_tmp:1} ` 
-log_dir=`echo ~/public_html/prompt_ntuple_log `
+log_dir=`echo /home/users/yanjuntu/public_html/prompt_ntuple_log `
 hadoop_dir=`echo /hadoop/cms/phedex `
 cd ${dataset_dir}
 
-#cat a.list |awk '{print $2}' |sed 's/\//\_/g' | sed 's/^/hadoop\_cms\_phedex/' > submit.list
-
-#cat a.list  |sed 's/\//\_/g' | sed 's/^/hadoop\_cms\_phedex/' > submit.list
 cat a.list  > submit.list
-#cat grouped.list |  cut -d"_" -f1-12 | sed 's/$/.root/'>  grouped.list.tmp 
 cat grouped.list |sed 's?_?/?g' |sed 's?store?/store?g'>  grouped.list.tmp
 cat submit.list | while read -r rn f; do
-    #grep ${f} grouped.list.tmp >& /dev/null || echo ${rn} ${f} | sed 's?hadoop_cms_phedex??' |sed 's?_?/?g' |sed -e 's?Nov4ReReco\/v1?Nov4ReReco_v1?g '; 
     grep ${f} grouped.list.tmp >& /dev/null || echo ${rn} ${f} ; 
-#done >& failed.list
 done >& $log_dir/${dataset_dir}_missing_files
 
 cat $log_dir/${dataset_dir}_missing_files| while read -r rn f; do
 f_hadoop=`echo $hadoop_dir$f `
 
-if test `find "$f_hadoop" -mmin +1200`
-    then
+if [ ! -s "$f_hadoop" ]; then
+    echo $rn $f
+elif test `find "$f_hadoop" -mmin +1200`; then
     echo $rn $f
 fi
 done >& $log_dir/${dataset_dir}_missing_files_20h
 
-[ ! -d "$log_dir/error" ] && mkdir $log_dir/error
+[ ! -f "a.list.resubmit" ]  && echo Create a.list.resubmit && touch a.list.resubmit
+cat $log_dir/${dataset_dir}_missing_files_20h |grep .root > a.runs.list0.tmp.resubmit
+'cp' a.list.resubmit a.list.old.resubmit
+'cp' a.runs.list0.tmp.resubmit a.list.resubmit
+'rm' -f a.list.new.resubmit; touch a.list.new.resubmit; grep store a.list.resubmit | while read -r rn f; do grep $f a.list.old.resubmit >& /dev/null || echo $rn $f >> a.list.new.resubmit; done
+aNewSize=`grep -c store a.list.new.resubmit`
+echo "Will resubmit ${aNewSize} files"
+cat a.list.new.resubmit
+nToSub=`grep -c store a.list.new.resubmit `
+if (( nToSub > 0 )) ; then
+    dateS=`date '+%Y.%m.%d-%H.%M.%S'`
+    subLog=sub.log.${dateS}
+    cp ../expressTools_UCSD_${dataset_dir}.cmd expressTools_UCSD_${dataset_dir}_resubmit.cmd
+    grep store a.list.new.resubmit | while read -r rn f; do 
+	input_data=`echo root://xrootd.unl.edu/$f `
+	python ../resubmit.py expressTools_UCSD_${dataset_dir}_resubmit.cmd $f
+	cd ${cur_dir}
+	condor_submit ${dataset_dir}/expressTools_UCSD_${dataset_dir}_resubmit.cmd
+	cd ${cur_dir}/${dataset_dir}
+    done >& submitting_log/${subLog}
+fi
 
+[ ! -d "$log_dir/error" ] && mkdir $log_dir/error
+chmod 777 $log_dir/error
 
 echo $dataset_name
 wc -l submit.list
 echo submitting Error or not finished jobs 
-#cat failed.list
 cat $log_dir/${dataset_dir}_missing_files
 echo merge Error
 ls -d merging_log/merging*| while read -r f; do
