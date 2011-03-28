406108c873074a0d1abdade370725453cc34f929
yanjuntu <yanjuntu>
2011-03-28 20:38:07 +0000

    fix the typo on dataset_name

--- allSkim.sh
+++ allSkim.sh
@@ -4,7 +4,7 @@ export EDG_WL_LOCATION=$VDT_LOCATION/edg
 source /data/vdt/setup.sh 
 
 cmssw_release=CMSSW_4_1_2_patch1
-cms2_tag=V04-00-08
+cms2_tag=V04-00-13
 
 #dataset_names="`echo /DoubleElectron/Run2011A-PromptReco-v1/AOD`"
 dataset_names="`echo /DoubleMu/Run2011A-PromptReco-v1/AOD` `echo /DoubleElectron/Run2011A-PromptReco-v1/AOD` "
@@ -13,7 +13,7 @@ while [ 1 ]
   do 
 
   for dataset_name in $dataset_names; do
-    source checkAndRunSkim.sh $cmssw_release $cms2_tag $dataset_names
+    source checkAndRunSkim.sh $cmssw_release $cms2_tag $dataset_name
   done
   sleep 7200 
 done
