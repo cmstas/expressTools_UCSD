f9e335848286db6c94252efb3121d77fa963b392
macneill <macneill>
2012-04-20 01:06:32 +0000

    Updated the time stamp on logging

--- allSkim.sh
+++ allSkim.sh
@@ -17,14 +17,14 @@ while [1]; do
 			DatasetDir_tmp=`echo $Dataset |sed -e 's?/?_?g' `
 			DatasetDir="${DatasetDir_tmp:1}"
 			touch /data/tmp/${USER}/${DatasetDir}/checkAndRunSkim.log && chmod a+r /data/tmp/${USER}/${DatasetDir}/checkAndRunSkim.log
-			./checkAndRunSkim.sh $Config $Dataset 2>&1 | appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndRunSkim.log  #don't run this process in the background, don't want to kill the system by skimming too many datasets at once
+			./checkAndRunSkim.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndRunSkim.log  #don't run this process in the background, don't want to kill the system by skimming too many datasets at once
 		done
 		sleep 15
 		for Dataset in $Datasets; do
 			DatasetDir_tmp=`echo $Dataset |sed -e 's?/?_?g' `
 			DatasetDir="${DatasetDir_tmp:1}"
 			touch /data/tmp/${USER}/${DatasetDir}/checkSkimErrors.log && chmod a+r /data/tmp/${USER}/${DatasetDir}/checkSkimErrors.log
-			./checkSkimErrors.sh $Config $Dataset 2>&1 | appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkSkimErrors.log &
+			./checkSkimErrors.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkSkimErrors.log &
 		done
 
 	done
