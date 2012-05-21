a4d6e8e5f7b57b7d1dbb29e0c06831ba07779417
macneill <macneill>
2012-05-21 06:29:42 +0000

    Now stores pids of child processes that get spawned and won't make a new iteration of that child process until the old one ends

--- allInOne.sh
+++ allInOne.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 #source some initial things for working with the grid 
 export VDT_LOCATION=/data/vdt
 export EDG_WL_LOCATION=$VDT_LOCATION/edg
@@ -18,7 +18,14 @@ while [ 1 ]; do
 			DatasetDir="${DatasetDir_tmp:1}"
 			mkdir -p /data/tmp/${USER}/${DatasetDir}
 			touch /data/tmp/${USER}/${DatasetDir}/checkAndSubmit.log && chmod a+r /data/tmp/${USER}/${DatasetDir}/checkAndSubmit.log
-			./checkAndSubmit.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndSubmit.log &
+			
+			pid=`echo $DatasetDir | sed -e 's/-/_/g'`_submit
+			eval "a=\$$pid"
+			kill -0 $a > /dev/null 2>&1
+			if [ $? != 0 ]; then
+				./checkAndSubmit.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndSubmit.log &
+				eval "$pid=$!"
+			fi
 		done
 		sleep 5400
 
@@ -28,7 +35,14 @@ while [ 1 ]; do
 			#echo $MergingDir
 		    #call the script to merge output files
 			touch /data/tmp/${USER}/${DatasetDir}/checkAndMerge.log && chmod a+r /data/tmp/${USER}/${DatasetDir}/checkAndMerge.log
-			./checkAndMerge.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndMerge.log & # changed to merge in the background 
+			
+			pid=`echo $DatasetDir | sed -e 's/-/_/g'`_merge
+			eval "a=\$$pid"
+			kill -0 $a > /dev/null 2>&1
+			if [ $? != 0 ]; then
+				./checkAndMerge.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkAndMerge.log & # changed to merge in the background 
+				eval "$pid=$!"
+			fi
 		done
 		sleep 5400
 
@@ -37,9 +51,17 @@ while [ 1 ]; do
 			DatasetDir="${DatasetDir_tmp:1}"
 			#call the script to check for erros in the merging step
 			touch /data/tmp/${USER}/${DatasetDir}/checkMergeErrors.log && chmod a+r /data/tmp/${USER}/${DatasetDir}/checkMergeErrors.log
-			./checkMergeErrors.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkMergeErrors.log &
+			
+			pid=`echo $DatasetDir | sed -e 's/-/_/g'`_error
+			eval "a=\$$pid"
+			kill -0 $a > /dev/null 2>&1
+			if [ $? != 0 ]; then
+				./checkMergeErrors.sh $Config $Dataset 2>&1 | ./appendTimeStamp.sh >> /data/tmp/${USER}/${DatasetDir}/checkMergeErrors.log &
+				eval "$pid=$!"
+			fi
 		done
-		sleep 
+		sleep 15
+
    	done
 done
 
